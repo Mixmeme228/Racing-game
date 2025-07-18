5f356157d4f6b700e326578887cb37483591588f
Mixmeme228 <64201621+Mixmeme228@users.noreply.github.com>
2025-07-19 01:04:58 +0300

    post processing

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class Speedometer : MonoBehaviour
 {
     public Rigidbody target;
-
+    public PostProcessProfile razmytie;
+    ChromaticAberration gg;
     public float maxSpeed = 0.0f; 
 
     public float minSpeedArrowAngle;
     public float maxSpeedArrowAngle;
-
+    
     [Header("UI")]
     public Text speedLabel;
     public RectTransform arrow;
@@ -20,9 +22,13 @@ public class Speedometer : MonoBehaviour
     private void Update()
     {
         speed = target.velocity.magnitude * 3.6f;
-
+        
         if (speedLabel != null)
+        {
             speedLabel.text = ((int)speed) + " km/h";
+            gg=razmytie.GetSetting<ChromaticAberration>();
+            gg.intensity.value=speed / 200 * 0.33f;
+        }
         if (arrow != null)
             arrow.localEulerAngles =
                 new Vector3(0, 0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle, speed / maxSpeed));
